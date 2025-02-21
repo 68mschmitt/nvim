14abbb11c5dcc44863bd960822d3e8138457a9f6
68mschmitt <68mschmitt@gmail.com>
2025-02-21 09:45:44 -0500

    Add regex to treesitter install defaults

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -2,7 +2,7 @@ require('nvim-treesitter.install').compilers = { 'zig' }
 
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "javascript", "typescript", "c_sharp", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json" },
+  ensure_installed = { "regex", "javascript", "typescript", "c_sharp", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
