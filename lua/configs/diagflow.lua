4b866e8dbd092330f3913ce1a335681199b2e799
68mschmitt <68mschmitt@gmail.com>
2025-06-11 09:58:01 -0400

    Diagflow message location updated

--- lua/configs/diagflow.lua
+++ lua/configs/diagflow.lua
@@ -1,11 +1,9 @@
 return {
     "dgagn/diagflow.nvim",
     enabled = true,
-    config = function()
-        require('diagflow').setup({
-            scope = 'line',
-            show_borders = true,
-            placement = "top",
-        })
-    end,
+    opts = {
+        scope = 'cursor',
+        show_borders = false,
+        placement = "inline",
+    },
 }
