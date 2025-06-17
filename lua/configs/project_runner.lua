97bd5336a59b86cbe051bd41b3ac84fa0611a8c2
68mschmitt <68mschmitt@gmail.com>
2025-06-17 12:25:54 -0400

    Kill projects before running new project

--- lua/configs/project_runner.lua
+++ lua/configs/project_runner.lua
@@ -1,11 +1,11 @@
 return {
     '68mschmitt/project_runner.nvim',
     lazy = false,
-    dev = true,
+    dev = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { autogenerate = false },
     init = function()
-        vim.api.nvim_set_keymap("n", "<leader>pr", "<cmd>ProjectRunnerSelect<CR>", { noremap = true, silent = true });
+        vim.api.nvim_set_keymap("n", "<leader>pr", "<cmd>ProjectRunnerKillAll<CR><cmd>ProjectRunnerSelect<CR>", { noremap = true, silent = true });
         vim.api.nvim_set_keymap("n", "<leader>pk", "<cmd>ProjectRunnerKillAll<CR>", { noremap = true, silent = true });
     end,
 }
